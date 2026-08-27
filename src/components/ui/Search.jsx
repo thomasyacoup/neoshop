09300a7829f730b28b844538b5ee310d2de6d83c
thomas011001 <thomasyacoub321@gmail.com>
2026-08-27 20:02:46 +0300

    feat: add Search component with styled input and button for enhanced user experience

---
+++ src/components/ui/Search.jsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+const StyledSearchBar = styled.div`
+  border: 3px solid ${({ theme }) => theme.colors.border};
+  box-shadow: ${({ theme }) => theme.shadow.md} ${({ theme }) => theme.colors.shadow};
+
+  display: none;
+
+  @media (min-width: ${({ theme }) => theme.media.tablet}) {
+    display: flex;
+  }
+  
+`
+
+const SearchInput = styled.input.attrs({type: "text"})`
+  padding: ${({ theme }) => theme.spacing.sm};;
+  max-width: 200px;
+  border: none;
+  outline: none;
+
+  &:focus {
+    background-color: ${({ theme }) => theme.colors.bg};;
+  }
+`
+
+const SearchBtn = styled.button`
+  border: none;
+  outline: none;
+  line-height: 0%;
+
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.onPrimary};
+  padding: ${({ theme }) => theme.spacing.sm};;
+  transition: 200ms;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+
+  &:active {
+    filter: brightness(0.5);
+  }
+`
+
+function Search({query, setQuery}) {
+  return (
+    <StyledSearchBar>
+      <SearchInput placeholder="Search..." value={query} onChange={e => setQuery(e.target.value)}/>
+      <SearchBtn>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" width="16px">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+
+      </SearchBtn>
+    </StyledSearchBar>
+  )
+}
+
+export default Search;
