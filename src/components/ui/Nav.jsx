823674a5631dad405832551ba41b7967a9b8b57c
thomas011001 <thomasyacoub321@gmail.com>
2026-08-27 16:59:06 +0300

    feat: implement Nav and CartLink components with styled navigation

---
+++ src/components/ui/Nav.jsx
@@ -0,0 +1,74 @@
+import { NavLink as Link } from "react-router-dom"
+import styled from "styled-components"
+
+const StyledNav = styled.nav`
+  display: flex;
+  gap: ${({ theme }) => theme.spacing.lg};
+`
+
+function Nav({children}) {
+  return (
+    <StyledNav>
+      {children}
+    </StyledNav>
+  )
+}
+
+const StyledNavLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.foreground};
+  font-size: ${({ theme }) => theme.fonts.size.md};
+  font-weight: ${({ theme }) => theme.fonts.weight.bold};
+  text-transform: uppercase;
+  text-decoration: none;
+  transition: 100ms;
+
+  &:hover, &.active {
+    color: color-mix(in oklab, ${({ theme }) => theme.colors.primary}, black 30%) !important;
+  }
+  
+  &:visited {
+    color: ${({ theme }) => theme.colors.foreground};
+  }
+`
+
+function NavLink({children, to}) {
+  return (
+    <StyledNavLink to={to}>
+      {children}
+    </StyledNavLink>
+  )
+}
+
+const StyledCartLink = styled(StyledNavLink)`
+  position: relative;
+
+  &::after {
+    content: "${props => props.itemsLength}";
+
+    color: ${({ theme }) => theme.colors.onPrimary};;
+    background-color: ${({ theme }) => theme.colors.primary};;
+    font-size: ${({ theme }) => theme.fonts.size.sm};;
+    font-family: monospace;
+    
+    padding: 2px;
+    line-height: 90%;
+    border: 2px solid ${({ theme }) => theme.colors.border};
+
+    position: absolute;
+    top: -8px;
+    right: -18px;
+  }
+`
+
+function CartLink({children, itemsLength, to}) {
+  return (
+    <StyledCartLink itemsLength={itemsLength} to={to}>
+      {children}
+    </StyledCartLink>
+  )  
+}
+
+Nav.NavLink = NavLink
+Nav.CartLink = CartLink
+
+export default Nav
