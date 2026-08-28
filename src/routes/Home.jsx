7ca1b2ee8a20df250c675c980a0d61e5b7dd0de8
thomas011001 <thomasyacoub321@gmail.com>
2026-08-28 17:02:01 +0300

    feat: implement Home component with hero section and image carousel

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,65 @@
+import styled from "styled-components";
+import { Container } from "../components/ui/Container";
+import Button from "../components/ui/Button";
+import { useNavigate } from "react-router-dom";
+import Carousel from "../components/ui/Carousel";
+
+const HomeContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: ${({ theme }) => theme.spacing.lg};
+`
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.xl};
+  padding: 96px 0;
+`
+const HeroTitle = styled.h1`
+  font-weight: ${({ theme }) => theme.fonts.weight.black};
+  font-size: ${({ theme }) => theme.fonts.size.xxxl};
+`
+const HeroP = styled.p`
+  color: ${({ theme }) => theme.colors.textMuted};
+  font-size: ${({ theme }) => theme.fonts.size.lg};
+`
+const HeroButton = styled(Button).attrs({size: "lg"})`
+  width: fit-content;
+  color: ${({ theme }) => theme.colors.foreground};
+`;
+
+const CarouselWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+`
+  
 function Home() {
-  return <div></div>;
+  const navigate = useNavigate()
+  const imgs = ["/src/assets/hero/hero-1.png", "/src/assets/hero/hero-2.png", "/src/assets/hero/hero-3.png"]
+  
+  
+  return (
+    <HomeContainer>
+      <TextWrapper>
+        <HeroTitle>
+          NEOSHOP: <br />
+          NO REGRETS
+        </HeroTitle>
+        <HeroP>
+          The boldest gear for the boldest people. <br />
+          Unapologetic style meets unyielding quality.
+        </HeroP>
+        <HeroButton onClick={() => navigate("/shop")}>
+          Shop Now
+        </HeroButton>
+      </TextWrapper>
+      <CarouselWrapper>
+        <Carousel imgs={imgs} n={3} />
+      </CarouselWrapper>
+    </HomeContainer>
+  );
 }
 
 export default Home;
