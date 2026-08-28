d549aefa962f0fe97b5a0fd9fc8c102e9df8e6dd
thomas011001 <thomasyacoub321@gmail.com>
2026-08-28 09:48:02 +0300

    feat: replace DrawerTrigger text with an SVG icon for improved visual clarity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,7 +71,11 @@ function Header({ cart }) {
           </Nav.CartLink>
         </Drawer>
         <Drawer.DrawerBackDrop display={activeDrawer ? "block" : "none"} onClick={handleToggleDrawer}/>
-        <Drawer.DrawerTrigger onClick={handleToggleDrawer}>O</Drawer.DrawerTrigger>
+        <Drawer.DrawerTrigger onClick={handleToggleDrawer}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" width="16px">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+          </svg>
+        </Drawer.DrawerTrigger>
       </HeaderContainer>
     </StyledHeader>
   )
