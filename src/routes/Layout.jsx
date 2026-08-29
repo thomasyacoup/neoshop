f552dd77607d8849179ca0f61b3a445c8e2766c0
thomas011001 <thomasyacoub321@gmail.com>
2026-08-29 07:45:51 +0300

    refactor: move styled Main component definition outside of Layout function for improved readability

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -4,14 +4,14 @@ import styled from "styled-components";
 import Header from "../components/Header.jsx"
 import Footer from "../components/Footer.jsx";
 
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
 function Layout() {
   const [cartProducts, setCartProducts] = useState([]);
 
-  const Main = styled.main`
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  `
 
   return (
     <Main>
